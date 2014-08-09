1c5bc8fc138026445bd8d50482e46c2bcfccdd4a
Lowell Bander <lowellbander@gmail.com>
2014-08-08 18:01:39 -0700

    now we're getting somewhere

--- csv_parse.py
+++ csv_parse.py
@@ -3,6 +3,14 @@ from collections import defaultdict
 
 def main():
     print "Hello, world!"
+    with open ('tour.csv', 'r') as handle:
+        doc = csv.DictReader(handle, ['blank', 'date', 'name', 'time',
+                                        'email', 'phone', 'comment', 'major'])
+        for family in doc:
+            if family['date'] == '11':
+                print family
+
+
 
 if __name__ == '__main__':
     main()
