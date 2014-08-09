9d3ea0577750dd1ae942001b3f80f77c702e124e
Lowell Bander <lowellbander@gmail.com>
2014-08-08 18:05:34 -0700

    done with prelim version

--- csv_parse.py
+++ csv_parse.py
@@ -2,13 +2,20 @@ import csv
 from collections import defaultdict
 
 def main():
-    print "Hello, world!"
     with open ('tour.csv', 'r') as handle:
         doc = csv.DictReader(handle, ['blank', 'date', 'name', 'time',
-                                        'email', 'phone', 'comment', 'major'])
+                                        'email', 'phone', 'comment', 'major', 'nPeople'])
         for family in doc:
             if family['date'] == '11':
-                print family
+                #print family
+                print "Name: %s" % family['name']
+                print "Email: %s" % family['email']
+                print "Phone Number: %s" % family['phone']
+                if family['comment'] != '':
+                    print "Comments: %s" % family['comment']
+                print "Major of Interest: %s" % family['major']
+                print "Number of People: %s" % family['nPeople']
+                print ""
 
 
 
