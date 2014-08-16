7c218a2469b39a1f3b6e067713a9dc7ed43419a3
Lowell Bander <lowellbander@gmail.com>
2014-08-16 10:56:59 -0700

    uploads csv to db

--- upload.py
+++ upload.py
@@ -1,9 +1,14 @@
 from pprint import pprint
+import csv
 import json
 import settings
+import sys
 import unirest
 
 HEADERS = {'Accept':'application/json', 'Content-Type': 'application-json'}
+
+def usage():
+    print "Usage: %s <tour.csv>" % sys.argv[0]
 
 def get_all():
     response = unirest.get(settings.API_URL + 'tour/')
@@ -14,11 +19,28 @@ def post(tour):
                                                 params = json.dumps(tour))
     return response.body
 
+
 def main():
-    #print "hello world"
-    #pprint(get_all())
-    tour = {'name': 'second'}
-    pprint(post(tour))
+
+    if len(sys.argv) is not 2:
+        usage()
+        return
+
+    document = sys.argv[1]
+
+    with open(document, 'r') as handle:
+        doc = csv.DictReader(handle, ['blank', 'date', 'name', 'time',
+                                    'email', 'phone', 'comment', 'major', 'nPeople'])
+        for family in doc:
+            print family
+            tour = {'date': family['date'] + " " + family['time'],
+                    'name': family['name'],
+                    'email': family['email'],
+                    'phone': family['phone'],
+                    'comment': family['comment'],
+                    'major_of_interest': family['major'],
+                    'nVisitors': family['nPeople']}
+            pprint(post(tour))
 
 if __name__ == "__main__":
     main()
