017612fd9ae8dca4dc56addfbe7da42ff318281a
Lowell Bander <lowellbander@gmail.com>
2014-08-16 11:52:55 -0700

    has subject, prints status msgs

--- followup.py
+++ followup.py
@@ -2,14 +2,24 @@ import smtplib
 import settings
 
 def main():
-    print "hello world"
     sender = "lowellbander@gmail.com"
     recipient = "lowellbander@gmail.com"
-    body =  "howdy"
+    subject = "Engineering Tour Follow-Up 123"
+    body =  """Dear ____,\n\nHow was the tour?\n\nSincerely,\nThe UCLA Engineering Ambassador Program"""
+    message = 'Subject: %s\n\n%s' % (subject, body)
+
+    print "Connecting to server ... ",
     server = smtplib.SMTP('smtp.gmail.com', 587)
+    print "done."
+    print "Starting TLS ...",
     server.starttls()
+    print "done."
+    print "Authenticating ...",
     server.login(settings.EMAIL_USER, settings.EMAIL_PASS)
-    server.sendmail(sender, recipient, body)
+    print "done."
+    print "Sending ... ",
+    server.sendmail(sender, recipient, message)
+    print "Sent!"
 
 
 if __name__ == "__main__":
